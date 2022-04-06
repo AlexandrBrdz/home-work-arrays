fefc4644f5e150217d5c17f88893a3b3993199e9
Александр Бордюжа <a.n.brdz@gmail.com>
2022-04-06 16:44:58 +0300

    третий комит

--- src/ru/scypro/Main.java
+++ src/ru/scypro/Main.java
@@ -1,10 +1,24 @@
 package ru.scypro;
 
 public class Main {
-
     public static void main(String[] args) {
 
-	// Задача 1
+        // Задача 1
+
+        int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+            System.out.println(arr[0] + "," + arr[1] + "," + arr[2]);
+
+
+        double[] weight = {1.57, 7.654, 9.986};
+        System.out.println(weight[0] + "," + weight[1] + "," + weight[2]);
+
+
+            char[] bricks = {70, 84, 87};
+            System.out.println(bricks[0] + "," + bricks[1] + "," + bricks[2]);
+
 
 
     }
